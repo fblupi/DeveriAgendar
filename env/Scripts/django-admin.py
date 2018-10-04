dc99743b9699cf418081f37ed3efae6972ac84b3
Fran Bolívar <francisco.bolivar@nazaries.com>
2018-10-04 19:44:15 +0200

    Updated .gitignore and delete local files

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Ordis\Arquivos\Desenvolvimento\ULBRA\2018.1\LPC\DeveriAgendar\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
